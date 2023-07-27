bdfb7495c40f0105ecdffa0bed650d9ee2c283b0
wutianhao <cnwutianhao@gmail.com>
2023-07-27 09:57:06 +0800

    英雄详情页内容丰富（基本信息、技能介绍、皮肤展示）

--- vscode/league-of-legends-assistant/src/heroTreeView.ts
+++ vscode/league-of-legends-assistant/src/heroTreeView.ts
@@ -72,6 +72,7 @@ export function registerHeroTreeView(context: vscode.ExtensionContext) {
                 const data = response.data;
                 const heroDetails = data.hero;
                 const skinDetails = data.skins;
+                const spellDetails = data.spells;
                 const panel = vscode.window.createWebviewPanel(
                     'heroDetails',
                     `英雄 - ${hero.name}`,
@@ -79,7 +80,19 @@ export function registerHeroTreeView(context: vscode.ExtensionContext) {
                     {}
                 );
 
-                // Create the HTML content of the webview panel with mainImg above shortBio
+                // 映射表将英文角色属性映射到对应的中文值
+                const roleMapping = new Map<string, string>([
+                    ['mage', '法师'],
+                    ['fighter', '战士'],
+                    ['tank', '坦克'],
+                    ['assassin', '刺客'],
+                    ['marksman', '射手'],
+                    ['support', '辅助'],
+                ]);
+
+                // 在生成 HTML 内容时，将英雄属性替换为中文值
+                const roles = heroDetails.roles.map((role: string) => roleMapping.get(role) || role).join('、');
+
                 const htmlContent = `
                 <!DOCTYPE html>
                 <html>
@@ -93,8 +106,54 @@ export function registerHeroTreeView(context: vscode.ExtensionContext) {
                 <body>
                     <h1>${hero.name}</h1>
                     <h2>${hero.title}</h2>
-                    <img src="${skinDetails[0].mainImg}" alt="${hero.name} Icon" style="max-width: 300px;">
+                    <img src="${skinDetails[0].mainImg}" alt="${hero.name} Icon" style="max-width: 500px;">
                     <p>${heroDetails.shortBio}</p>
+                    <hr>
+
+                    <h3>版本</h3>
+                    当前版本 ${data.version}
+                    <br />更新时间 ${data.fileTime}
+                    <hr>
+
+                    <h3>基本信息</h3>
+                    英雄属性 ${roles}
+                    <br />生命值 ${parseFloat(heroDetails.hp).toFixed(3).replace(/\.?0+$/, '')}
+                    <br />生命值回复 ${parseFloat(heroDetails.hpregen).toFixed(3).replace(/\.?0+$/, '')}
+                    <br />魔法值 ${parseFloat(heroDetails.mp).toFixed(3).replace(/\.?0+$/, '')}
+                    <br />魔法值回复 ${parseFloat(heroDetails.mpregen).toFixed(3).replace(/\.?0+$/, '')}
+                    <br />移动速度 ${parseFloat(heroDetails.movespeed).toFixed(3).replace(/\.?0+$/, '')}
+                    <br />攻击范围 ${parseFloat(heroDetails.attackrange).toFixed(3).replace(/\.?0+$/, '')}
+                    <br />攻击力 ${parseFloat(heroDetails.attackdamage).toFixed(3).replace(/\.?0+$/, '')}
+                    <br />攻击速度 ${parseFloat(heroDetails.attackspeed).toFixed(3).replace(/\.?0+$/, '')}
+                    <br />护甲 ${parseFloat(heroDetails.armor).toFixed(3).replace(/\.?0+$/, '')}
+                    <br />魔法抗性 ${parseFloat(heroDetails.spellblock).toFixed(3).replace(/\.?0+$/, '')}
+                    <hr>
+
+                    <h3>技能</h3>
+                    ${spellDetails.map((spell: any) => {
+                    // 将英文的 "passive" 转换为中文 "被动"
+                    const spellKeyChinese = spell.spellKey === "passive" ? "被动" : spell.spellKey;
+                    return `
+                    <h4>${spell.name}（${spellKeyChinese}）</h4>
+                    <img src="${spell.abilityIconPath}" alt="${spell.name} Icon" style="max-width: 100px;">
+                    <p>${spell.description}</p>
+                    `;
+                    }).join('')}
+                    <hr>
+
+                    <h3>皮肤</h3>
+                    ${skinDetails.map((skin: any) => {
+                    // 检查 "chromasBelongId" 是否为 "0"，如果是则显示该皮肤图像
+                    if (skin.chromasBelongId === "0") {
+                    return `
+                    <h4>${skin.name}</h4>
+                    <img src="${skin.mainImg}" alt="${skin.name} Skin" style="max-width: 300px;">
+                    `;
+                    }
+                    // 如果不满足条件，则返回空字符串，不显示该皮肤图像
+                    return '';
+                    }).join('')}
+                    <hr>
                 </body>
                 
                 </html>
