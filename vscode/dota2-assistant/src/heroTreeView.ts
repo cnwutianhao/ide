1e4dfc632c465747bd2bc5c1aad323b73720c429
wutianhao <cnwutianhao@gmail.com>
2023-08-04 16:42:40 +0800

    英雄详情修改

--- vscode/dota2-assistant/src/heroTreeView.ts
+++ vscode/dota2-assistant/src/heroTreeView.ts
@@ -10,6 +10,13 @@ interface Hero {
     complexity: number;
 }
 
+interface Ability {
+    id: number;
+    name: string;
+    name_loc: string;
+    desc_loc: string;
+}
+
 class HeroProvider implements vscode.TreeDataProvider<Hero> {
     private _onDidChangeTreeData = new vscode.EventEmitter<Hero | undefined>();
     readonly onDidChangeTreeData = this._onDidChangeTreeData.event;
@@ -72,6 +79,8 @@ export function registerHeroTreeView(context: vscode.ExtensionContext) {
                     {}
                 );
 
+                const heroAbilities = heroData.abilities;
+
                 const htmlContent = `
                 <!DOCTYPE html>
                 <html>
@@ -84,10 +93,23 @@ export function registerHeroTreeView(context: vscode.ExtensionContext) {
                 
                 <body>
                   <h1>${heroData.name_loc}</h1>
-                  <img src="https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/${heroData.name.replace('npc_dota_hero_', '')}.png">
+                  <img src="https://cdn.cloudflare.steamstatic.com/apps/dota2/videos/dota_react/heroes/renders/${heroData.name.replace('npc_dota_hero_', '')}.png" style="max-width: 500px;">
+                
+                  <h2>背景</h2>
                   <p>${heroData.bio_loc}</p>
                   <p>${heroData.hype_loc}</p>
                   <p>${heroData.npe_desc_loc}</p>
+                
+                  <h2>技能</h2>
+                  <ul>
+                    ${heroAbilities.map((ability: Ability) => `
+                    <li>
+                      <img src="https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/abilities/${ability.name}.png" style="max-width: 80px;">
+                      <h3>${ability.name_loc}</h3>
+                      <p>${ability.desc_loc}</p>
+                    </li>
+                    `).join('')}
+                  </ul>
                 </body>
                 
                 </html>
